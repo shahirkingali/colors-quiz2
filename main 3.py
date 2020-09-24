223fb4008d48034dfa38fb0dc79b5dd917c05f8a
Shahir Ali <shahirsheikali@gmail.com>
2020-09-24 07:55:59 -0700

    Add files via upload

---
+++ main 3.py
@@ -0,0 +1,10 @@
+# Create a list called colors which will contain 5 strings with different colors: "red", "yellow", "green", "blue", "purple
+# Use the .append() method or command to add the color "violet" to the color list
+# Delete the 2nd item in the colors list
+# Write a loop that will print out all the colors in the colors list
+colors = ["red", "yellow", "green", "blue", "purple"]
+print( colors )
+colors.append("violet")
+print( colors )
+colors.remove('yellow')
+print(colors)
